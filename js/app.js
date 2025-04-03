d43f640e583552d5cb2c0a3d51115f92330cc972
Erik Borges <erikborgesofficial@gmail.com>
2025-04-02 23:43:48 -0300

    Ultima atualização

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 let ingressosRestantes = {
     'pista': {quantidade: 0},
     'superior': {quantidade: 0},
@@ -8,27 +7,23 @@ let ingressosRestantes = {
 function comprar() {
     let opcoesDeIngressos = document.getElementById('tipo-ingresso').value;
     let quantidadeDesejada = Number(document.getElementById('qtd').value);
-
     let quantidade = Number(document.getElementById(`qtd-${opcoesDeIngressos}`).textContent);
     ingressosRestantes[opcoesDeIngressos].quantidade = quantidade;
-
 
     if (quantidadeDesejada > ingressosRestantes[opcoesDeIngressos].quantidade) {
         alert('Quantidade de ingresso indisponível')
     } else {
         if (opcoesDeIngressos == 'inferior') {
             document.getElementById('qtd-inferior').innerHTML = quantidade - quantidadeDesejada;
+            alert('Compra realizada com secesso!')
         } else if (opcoesDeIngressos == 'pista') {
             document.getElementById('qtd-pista').innerHTML = quantidade - quantidadeDesejada;
-
-    
+            alert('Compra realizada com secesso!')
         } else if (opcoesDeIngressos == 'superior') {
             document.getElementById('qtd-superior').innerHTML = quantidade - quantidadeDesejada;
+            alert('Compra realizada com secesso!')
 
         }
-        document.getElementById('qtd').value = '';
-
     }
-console.log(quantidadeDesejada);
-
+    document.getElementById('qtd').value = '';
 }
