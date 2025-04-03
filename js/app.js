16a3325121bf64f53951c9d56afcfbc706c1e8cb
erikborges013 <erikborgesofficial@gmail.com>
2025-04-03 01:04:55 -0300

    correção de bug app.js

--- js/app.js
+++ js/app.js
@@ -10,18 +10,18 @@ function comprar() {
     let quantidade = Number(document.getElementById(`qtd-${opcoesDeIngressos}`).textContent);
     ingressosRestantes[opcoesDeIngressos].quantidade = quantidade;
 
-    if (quantidadeDesejada > ingressosRestantes[opcoesDeIngressos].quantidade) {
-        alert('Quantidade de ingresso indisponível')
+    if (quantidadeDesejada > ingressosRestantes[opcoesDeIngressos].quantidade || quantidadeDesejada <= 0) {
+        alert(`Por favor! Escolha uma quantidade de ingresso disponível. Quantidade disponível ${ingressosRestantes[opcoesDeIngressos].quantidade} unidades.`)
     } else {
         if (opcoesDeIngressos == 'inferior') {
             document.getElementById('qtd-inferior').innerHTML = quantidade - quantidadeDesejada;
-            alert('Compra realizada com secesso!')
+            alert('Compra realizada com sucesso!')
         } else if (opcoesDeIngressos == 'pista') {
             document.getElementById('qtd-pista').innerHTML = quantidade - quantidadeDesejada;
-            alert('Compra realizada com secesso!')
+            alert('Compra realizada com sucesso!')
         } else if (opcoesDeIngressos == 'superior') {
             document.getElementById('qtd-superior').innerHTML = quantidade - quantidadeDesejada;
-            alert('Compra realizada com secesso!')
+            alert('Compra realizada com sucesso!')
 
         }
     }
